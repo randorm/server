13fc1defbe03ad9381411a14f0a46f20ef280e25
Egor Machnev <egorikhelp@gmail.com>
2022-08-02 16:18:02 +0300

    Update res.py

--- res.py
+++ res.py
@@ -1,14 +1,16 @@
 import asyncio
+import logging
 from asyncio import sleep
 from enum import Enum
 from json import dumps
 from os import getenv
 
+import aioredis
 from aiogram import Bot
+from aiogram.exceptions import TelegramForbiddenError
 from aiogram.types import InlineKeyboardButton
 from aiogram.utils.keyboard import InlineKeyboardBuilder
-from aioredis import from_url
-from pydantic import BaseModel
+from pydantic import BaseModel, ValidationError
 
 COOLDOWN = 3
 
@@ -29,9 +31,9 @@ class Student(Node):
     age: int
 
 
-async def main(token: str, fsm: str):
+async def main(token: str, redis_url: str):
     bot = Bot(token)
-    redis = from_url(fsm, decode_responses=True)
+    redis = aioredis.from_url(redis_url, decode_responses=True)
 
     students = {}
     for state_key in await redis.keys("fsm:*:state"):
@@ -39,7 +41,12 @@ async def main(token: str, fsm: str):
         if state == "Form:subscribe":
             data_key = state_key.replace("state", "data")
             data = await redis.get(data_key)
-            student = Student.parse_raw(data)
+
+            try:
+                student = Student.parse_raw(data)
+            except ValidationError as error:
+                logging.warning(error)
+                continue
 
             chat, user = map(int, state_key.split(":")[1:-1])
             try:
@@ -48,15 +55,17 @@ async def main(token: str, fsm: str):
                     f"{student.name}, the second phase has "
                     "come to an end. We're glad you took part!",
                     reply_markup=InlineKeyboardBuilder().row(
-                        InlineKeyboardButton(
-                            text="Support",
-                            url="tg://user?id=709491996"
-                        )
+                        InlineKeyboardButton(text="Support",
+                                             url="tg://user?id=709491996")
                     ).as_markup()
                 )
-                await sleep(COOLDOWN)
-            finally:
+            except TelegramForbiddenError as error:
+                logging.warning(error)
+                continue
+            else:
                 students |= {user: student}
+            finally:
+                await sleep(COOLDOWN)
 
     with open("students.json", "w") as file:
         file.write(dumps(students))
