2a5c2f6cb338589fae2031b59ca018fe0e1cce23
machnevegor <egorikhelp@gmail.com>
2023-06-15 11:08:53 +0300

    fix(api): update assertUserProfile

--- api/assert/assert_user_profile.ts
+++ api/assert/assert_user_profile.ts
@@ -1,19 +1,19 @@
 import { GraphQLError } from "../deps.ts";
-import type { UserModel } from "../model/mod.ts";
+import type { ProfileModel } from "../model/mod.ts";
 
-export function assertUserProfile(user: UserModel) {
-  if (!user.profile.firstName || user.profile.firstName.length > 32) {
+export function assertUserProfile(profile: ProfileModel) {
+  if (!profile.firstName || profile.firstName.length > 32) {
     throw new GraphQLError("First name should be between 1 and 32 characters");
   }
-  if (!user.profile.lastName || user.profile.lastName.length > 32) {
+  if (!profile.lastName || profile.lastName.length > 32) {
     throw new GraphQLError("Last name should be between 1 and 32 characters");
   }
 
-  if (user.profile.birthday > new Date()) {
+  if (profile.birthday > new Date()) {
     throw new GraphQLError("Birthday should be in the past");
   }
 
-  if (!user.profile.bio || user.profile.bio.length > 256) {
+  if (!profile.bio || profile.bio.length > 256) {
     throw new GraphQLError("Bio should be between 1 and 256 characters");
   }
 }
