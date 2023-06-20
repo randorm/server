a3ca2d56d3eebffc1582d4eb6b4b5347534bd5af
machnevegor <egorikhelp@gmail.com>
2023-06-20 21:59:08 +0300

    feat(server): update me operation

--- server/operation/user.ts
+++ server/operation/user.ts
@@ -21,6 +21,10 @@ import { asyncMap } from "../util/mod.ts";
 export const UserQuery: Operation = new GraphQLObjectType({
   name: "Query",
   fields: {
+    me: {
+      type: new GraphQLNonNull(UserNode),
+      resolve: (_root, _args, { user }) => user,
+    },
     user: {
       type: new GraphQLNonNull(UserNode),
       args: {
@@ -37,10 +41,6 @@ export const UserQuery: Operation = new GraphQLObjectType({
 
         return res.value;
       },
-    },
-    me: {
-      type: new GraphQLNonNull(UserNode),
-      resolve: (_root, _args, { user }) => user,
     },
     userCount: {
       type: new GraphQLNonNull(GraphQLInt),
