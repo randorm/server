2acfcf537a6f5874e08851528fa72fa223e12e7f
machnevegor <egorikhelp@gmail.com>
2023-07-11 00:01:50 +0300

    feat: add authentication route

--- routes/mod.ts
+++ routes/mod.ts
@@ -1,8 +1,12 @@
 import { Router } from "../deps.ts";
 import type { ServerContext } from "../types.ts";
+import { router as authorization } from "./authentication.ts";
 import { router as graphql } from "./graphql.ts";
 
 export const router = new Router<ServerContext>();
 
 router.use(graphql.routes());
 router.use(graphql.allowedMethods());
+
+router.use(authorization.routes());
+router.use(authorization.allowedMethods());
