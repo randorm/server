1b9778ec03f3932c82e5df8f20f204c228f5e3e9
machnevegor <egorikhelp@gmail.com>
2023-07-10 23:57:04 +0300

    feat: add validators

--- utils/validation.ts
+++ utils/validation.ts
@@ -1,5 +1,15 @@
 import { z } from "../deps.ts";
 
+export const AuthenticationData = z.object({
+  id: z.number(),
+  first_name: z.string(),
+  last_name: z.string().optional(),
+  username: z.string().optional(),
+  photo_url: z.string().optional(),
+  auth_date: z.number(),
+  hash: z.string(),
+});
+
 export const GraphQLParams = z.object({
   query: z.string(),
   variables: z.record(z.unknown()).nullish(),
