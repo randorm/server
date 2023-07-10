4419d1f93250e5154935f00b8d7c7dd15e9ca9e7
machnevegor <egorikhelp@gmail.com>
2023-07-10 23:56:37 +0300

    feat: update deps

--- deps.ts
+++ deps.ts
@@ -1,10 +1,14 @@
 export { chunk } from "https://deno.land/std@0.193.0/collections/chunk.ts";
+export { mapValues } from "https://deno.land/std@0.193.0/collections/map_values.ts";
 export { zip } from "https://deno.land/std@0.193.0/collections/zip.ts";
+export { MINUTE } from "https://deno.land/std@0.193.0/datetime/constants.ts";
 export { difference } from "https://deno.land/std@0.193.0/datetime/difference.ts";
 export { format } from "https://deno.land/std@0.193.0/datetime/format.ts";
 export { parse } from "https://deno.land/std@0.193.0/datetime/parse.ts";
 export { oakCors } from "https://deno.land/x/cors@v1.2.2/mod.ts";
+export { create, verify } from "https://deno.land/x/djwt@v2.9/mod.ts";
 export { Bot, Composer } from "https://deno.land/x/grammy@v1.17.1/mod.ts";
+export { checkSignature } from "https://deno.land/x/grammy_validator@v1.0.1/mod.ts";
 export {
   Application,
   Router,
