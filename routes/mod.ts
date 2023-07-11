05af0372154c6d747bad7fb7df1baebe093019da
machnevegor <egorikhelp@gmail.com>
2023-07-11 18:56:07 +0300

    fix: update route name

--- routes/mod.ts
+++ routes/mod.ts
@@ -1,6 +1,6 @@
 import { Router } from "../deps.ts";
 import type { ServerContext } from "../types.ts";
-import { router as authorization } from "./authentication.ts";
+import { router as authentication } from "./authentication.ts";
 import { router as graphql } from "./graphql.ts";
 
 export const router = new Router<ServerContext>();
@@ -8,5 +8,5 @@ export const router = new Router<ServerContext>();
 router.use(graphql.routes());
 router.use(graphql.allowedMethods());
 
-router.use(authorization.routes());
-router.use(authorization.allowedMethods());
+router.use(authentication.routes());
+router.use(authentication.allowedMethods());
