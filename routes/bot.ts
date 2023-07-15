5677ac000b63550a289b34ea14eecdd6baf0153c
machnevegor <egorikhelp@gmail.com>
2023-07-15 17:25:54 +0300

    feat: update telegram webhook route

--- routes/bot.ts
+++ routes/bot.ts
@@ -3,6 +3,6 @@ import type { ServerContext } from "../types.ts";
 
 export const router = new Router<ServerContext>();
 
-router.all("/bot", (ctx) => {
-  return ctx.state.webhook(ctx.request);
+router.post("/bot", (ctx) => {
+  return ctx.state.webhook(ctx);
 });
