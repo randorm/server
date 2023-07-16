e0f46e30243205cd944b68f29db0f491acdebc8e
Andrew <jandr21@ya.ru>
2023-07-17 02:22:56 +0300

    fix: minor bug

--- services/bot/bot.ts
+++ services/bot/bot.ts
@@ -1044,7 +1044,7 @@ composer.on("callback_query:data", async (ctx: BotContext) => {
       //     reply_markup: undefined,
       //   },
       // );
-      if (ctx.session.fieldCurrentIndex === ctx.session.fieldAmount && ctx.session.distributionId !== undefined) {
+      if (ctx.session.fieldsIds.length === 1 && ctx.session.distributionId !== undefined) {
         await ctx.reply("Yooo congratulations, you finished! Now use /feed")
         ctx.session.fieldStep = FieldStep.FINISH;
         ctx.session.answeredQuestions = true;
