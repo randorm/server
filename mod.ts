86a7a76e75bd73b02a8d9a9123750fc90b9dc680
Andrew <jandr21@ya.ru>
2023-07-24 01:22:04 +0300

    feat: cleaning database [2/3]

--- mod.ts
+++ mod.ts
@@ -1,10 +1,134 @@
+import {
+  Application,
+  Bot,
+  DenoKVAdapter,
+  oakCors,
+  session,
+  webhookCallback,
+} from "./deps.ts";
+import { router } from "./routes/mod.ts";
+import type { SessionData } from "./services/bot/mod.ts";
+import { composer } from "./services/bot/mod.ts";
+import type { BotContext, ServerContext } from "./types.ts";
+import {
+  PORT,
+  setupKeys,
+  setupState,
+  STORAGE_KEY_GROUPS,
+  STORAGE_VERSION,
+} from "./utils/mod.ts";
+
+// Step 1.1. Create a connection to the database.
+
 const kv = await Deno.openKv();
 
-Deno.serve(async () => {
-  const iter = kv.list({ prefix: [] });
-  for await (const { key } of iter) {
-    await kv.delete(key);
-  }
+// Step 1.2. Setup the database keys.
 
-  return new Response("Done");
+await setupKeys(kv, STORAGE_VERSION, STORAGE_KEY_GROUPS);
+
+////////////////////////////////////////////////////////////////
+
+// Step 2.1. Get a Telegram Bot token.
+
+const BOT_TOKEN = Deno.env.get("BOT_TOKEN");
+
+if (!BOT_TOKEN) {
+  throw new Error("Missing `BOT_TOKEN` environment variable");
+}
+
+// Step 2.2. Create a Telegram Bot instance.
+
+const bot = new Bot<BotContext>(BOT_TOKEN);
+
+////////////////////////////////////////////////////////////////
+
+// Step 3.1. Create a webhook callback.
+
+const webhook = webhookCallback(bot, "oak");
+
+// Step 3.2. Create a session middleware.
+
+const sessionMiddleware = session<SessionData, BotContext>({
+  initial: () => ({}),
+  storage: new DenoKVAdapter(kv),
 });
+
+////////////////////////////////////////////////////////////////
+
+// Step 4.1. Get a JWK.
+
+const JWK = Deno.env.get("JWK");
+
+if (!JWK) {
+  throw new Error("Missing `JWK` environment variable");
+}
+
+// Step 4.2. Create a CryptoKey instance.
+
+const jwk = await crypto.subtle.importKey(
+  "jwk",
+  JSON.parse(JWK),
+  { name: "HMAC", hash: "SHA-512" },
+  true,
+  ["sign", "verify"],
+);
+
+////////////////////////////////////////////////////////////////
+
+// Step 5. Create a ServerContext object.
+
+const state: ServerContext = {
+  kv,
+  botToken: BOT_TOKEN,
+  bot,
+  jwk,
+  webhook,
+};
+
+////////////////////////////////////////////////////////////////
+
+// Step 6.1. Register bot state middleware.
+
+bot.use(setupState(state));
+
+// Step 6.3. Register session middleware.
+
+bot.use(sessionMiddleware);
+
+// Step 6.3. Register bot handlers.
+
+bot.use(composer);
+
+////////////////////////////////////////////////////////////////
+
+// Step 7.1. Create an Application instance.
+
+const app = new Application<ServerContext>({ contextState: "alias", state });
+
+// Step 7.2.1. Register CORS middleware.
+
+app.use(oakCors());
+
+// Step 7.2.2. Register application routes.
+
+app.use(router.routes());
+app.use(router.allowedMethods());
+
+////////////////////////////////////////////////////////////////
+
+// Step 8.1. Get the server origin.
+
+const ORIGIN = Deno.env.get("ORIGIN");
+
+if (!ORIGIN) {
+  throw new Error("Missing `ORIGIN` environment variable");
+}
+
+const WEBHOOK_URL = new URL("/bot", ORIGIN);
+
+// Step 8.2. Start the server.
+
+await Promise.all([
+  app.listen({ port: PORT }),
+  bot.api.setWebhook(WEBHOOK_URL.toString()),
+]);
